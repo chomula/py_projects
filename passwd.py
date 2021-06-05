aa45abcb40181387317a466e7ddcd0c8b6c0bcc3
chomula <chomulacomputers@gmail.com>
2021-06-05 19:43:29 +0200

    added dictionary check of entered password

--- passwd.py
+++ passwd.py
@@ -5,19 +5,24 @@ Created on Wed Jun  2 15:52:51 2021
 @author: cmulanshi
 """
 
+
+
 def homePage():
+    pass
     print("Welcome to the password tester program")
     print("Please enter your password for strength testing")
     print("************************************************")
     passwd = input(">")
-    if lengthCheck(passwd) and charCheck(passwd) and upperCheck(passwd) and lowerCheck(passwd) and checkNum(passwd) == True:
+    if lengthCheck(passwd) and charCheck(passwd) and upperCheck(passwd) and lowerCheck(passwd) and checkNum(passwd) and dictCheck(passwd) == True:
         print(f"This Password {passwd} is very strong")
 def lengthCheck(passwd):
     """Assumes passwd is a string, checks if length is greater than 10 
        Returns True if yes and None if false"""
     if len(passwd) < 10 or len(passwd) < 0:
         print("Invalid password length, your password is too short")
+        print("Enter a password with atleast 10 characters.")
         homePage()
+        return False
     else:
         return True
         
@@ -34,6 +39,7 @@ def charCheck(passwd):
     if has_char == False:
         print("Your password has NO special characters e.g @#")
         homePage()
+        return False
     else:
         return True
     
@@ -50,6 +56,7 @@ def upperCheck(passwd):
     if has_upper == False:
         print("There are NO capital letters in your password")
         homePage()
+        return False
     else:
         return True
     
@@ -66,6 +73,7 @@ def lowerCheck(passwd):
     if has_lower == False:
         print("There are no lower case letters in your pass word")
         homePage()
+        return False
     else:
         return True
     
@@ -86,7 +94,26 @@ def checkNum(passwd):
     if has_num == False:
         print("Your password must atleast contain a number")
         homePage()
+        return False
     else:
         return True
-            
+
+def dictCheck(passwd):
+    """Assumes passwd is a string, checks to see if passwd in contained in dictionary data
+       Returns False if yes and True if not """
+    passList = list()
+    fhandle = open("dict.txt",'r')
+    for line in fhandle.readlines():
+        passList.append(line.strip("\n"))
+    if passwd in passList:
+        print("WARNING!!Your password was found in a local dictionary")
+        homePage()
+        return False
+    else:
+        fhandle2 = open("dict.txt",'a')
+        if fhandle2.write(passwd + "\n"):
+            pass
+        return True
+
+
 homePage()
